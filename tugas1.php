2f33ee52c0f0bcbba2327446a81bc4c37a930751
intansahara03 <109254804+intansahara03@users.noreply.github.com>
2022-08-24 15:01:32 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,11 @@
+<?php
+//pembuatan fungsi
+$panjang =18;
+$lebar =10;
+$tinggi =12;
+function VolumeBalok ($panjang, $lebar,$tinggi) {
+    return ($panjang*$lebar*$tinggi);
+}
+//menampilkan fungsi
+echo "Volume balok tersebut adalah :" .VolumeBalok($panjang, $lebar,$tinggi). "cm";
+?>
